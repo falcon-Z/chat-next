00321a614bc840c4fb00de190a0cffdb23f9fe9f
sai madhan <saimadhan@outlook.in>
2023-06-06 19:42:46 +0530

    setup appwrite

---
+++ src/app/_utils/config.ts
@@ -0,0 +1,7 @@
+import { Account, Client } from "appwrite";
+
+export const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`);
+
+export const account = new Account(client);
