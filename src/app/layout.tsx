1932c1d34bbac81bbd157a61d767492358f947df
sai madhan <saimadhan@outlook.in>
2023-06-07 16:33:57 +0530

    setup tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import QueryProvider from "./_utils/queryProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -9,10 +10,16 @@ export const metadata = {
     "Looking for some fun and inspiration? Join our random developer chat and meet a new buddy who shares your passion for coding. You can exchange ideas, jokes, tips, or even argue about why Javascript is the best (or worst) language ever. Don't be shy, just say hi and start a conversation with a fellow developer. ",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
